75f9476595532a8753d3e3d7f0dcfba4d521c417
Nick Rizzo <nickrizzo77277@gmail.com>
2021-11-29 11:58:22 -0600

    created writetofile function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log("writetofile data", data);
+  fs.writeToFile(fileName, data, (err) => {
+    if (err) {
+      console.log("There was a Problem. Please try again.");
+    } else {
+      console.log("File Successfully Created");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
